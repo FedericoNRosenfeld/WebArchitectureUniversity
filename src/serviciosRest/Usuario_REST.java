48a30a4ec167c8c8179c8672f0e86f23daab51fb
elmuddy <fn.rosenfeld@gmail.com>
2017-10-28 17:04:32 -0300

    Rest_Usuarios : getUsuarios

--- src/serviciosRest/Usuario_REST.java
+++ src/serviciosRest/Usuario_REST.java
@@ -1,8 +1,22 @@
 package serviciosRest;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import entidades.Usuario;
+import entities.User;
+import services.DAOUser;
 
 @Path("/usuarios")
 public class Usuario_REST {
-
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getUsers() {
+		List<User> result = DAOUsuario.getInstance().getUsuarios();
+		return result;
+	}
 }
